006394a80b4832147b672f86535835d178e8148f
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-10-15 09:12:45 -0300

    ✔ Added: tailwind css

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,18 +1,4 @@
+/* eslint-env node */
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+  extends: ['@claudiobzrr/eslint-config/react'],
+};
