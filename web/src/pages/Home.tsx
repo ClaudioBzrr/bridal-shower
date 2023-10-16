280b41d2806c62a50089a1b5f012549796d8db86
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-10-15 23:02:26 -0300

    ✔ Added: underline hover effect

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -14,7 +14,7 @@ export function Home() {
       <section className="flex  h-[16em] w-[20em] flex-col items-center justify-evenly text-slate-50 sm:h-[10em] sm:w-[40em] sm:justify-between">
         <div id="location" className=" font-satisfy flex flex-col  text-center text-2xl sm:flex-row sm:text-3xl">
           <div >18/11 às 18:00 hrs no </div>
-          <div className='sm: ml-2'><a className="flex flex-row text-green-700 underline" target="__blank" href="https://maps.app.goo.gl/GGCJYrEpCwuUN5Kg6"><FaLocationDot/>Floresta Bar</a></div>
+          <div className='sm: ml-2'><a className="flex flex-row text-green-700 hover:underline" target="__blank" href="https://maps.app.goo.gl/GGCJYrEpCwuUN5Kg6"><FaLocationDot/>Floresta Bar</a></div>
         </div>
         <div id="confirm-presence">
             <button className="font-amatic flex flex-row items-center rounded-full bg-red-400 p-3 text-2xl font-bold sm:p-5 sm:text-3xl">Quero participar<FiChevronsRight/></button>
