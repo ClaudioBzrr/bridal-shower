6ea929d8a5c933df734509b475ac66afb5a1d5d3
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2023-10-18 14:54:32 +0000

    ✔️ Added: right and left element support in button component

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -4,17 +4,20 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 type IButton = ButtonHTMLAttributes<HTMLButtonElement> & {
   title: string;
-  icon?: React.JSX.Element;
+  rightElement?: React.JSX.Element;
+  leftElement?: React.JSX.Element;
 };
 
-export function Button({ title, icon, ...rest }: IButton) {
+export function Button({ title, leftElement, rightElement, ...rest }: IButton) {
   return (
     // <div className="flex w-full flex-row items-center justify-center">
     <button
       className="0.5s w-full rounded-full bg-red-400 p-1 text-center font-amatic text-2xl font-bold transition-all ease-in-out hover:bg-red-500 sm:px-8"
       {...rest}
     >
+      {rightElement ? <div className="pl-1">{rightElement}</div> : null}
       {title}
+      {leftElement ? <div className="pr-1">{rightElement}</div> : null}
     </button>
     // </div>
   );
