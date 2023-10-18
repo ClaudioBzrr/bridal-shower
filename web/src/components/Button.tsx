b67c86832ff18c64965e00a4723325636aac553e
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2023-10-18 14:17:49 +0000

    🩹 Fixed: button text to center

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -11,7 +11,7 @@ export function Button({ title, icon, ...rest }: IButton) {
   return (
     // <div className="flex w-full flex-row items-center justify-center">
     <button
-      className="0.5s flex w-full flex-row items-center rounded-full bg-red-400 p-1 text-center font-amatic text-2xl font-bold transition-all ease-in-out hover:bg-red-500 sm:px-8"
+      className="0.5s w-full rounded-full bg-red-400 p-1 text-center font-amatic text-2xl font-bold transition-all ease-in-out hover:bg-red-500 sm:px-8"
       {...rest}
     >
       {title}
