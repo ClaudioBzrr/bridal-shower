77de541b958d8dabef5c93a8e3f80cac6079836f
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-10-23 23:02:54 -0300

    ✔ Added: switch component while confirm event step finished

--- web/src/pages/Event/Index.tsx
+++ web/src/pages/Event/Index.tsx
@@ -1,5 +1,13 @@
+import { useState } from 'react';
+import { PickListStep } from './Steps/PickListStep';
+import { ConfirmStep } from './Steps/ConfirmStep';
 export function Event() {
+  const [eventconfirm, setEventConfirm] = useState<boolean>(false);
   return (
-    <main className="flex h-[100vh] max-h-[100vh] w-full max-w-[100vw] flex-col items-center justify-center bg-slate-900"></main>
+    <main className="flex h-[100vh] max-h-[100vh] w-full max-w-[100vw] flex-col items-center justify-center bg-slate-900">
+      <div className="max-w-xs sm:max-w-sm">
+        {eventconfirm == true ? <PickListStep /> : <ConfirmStep />}
+      </div>
+    </main>
   );
 }
