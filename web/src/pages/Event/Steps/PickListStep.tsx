6250d161d938bc5aee1fdecde895a38f7dc56b84
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-10-25 21:29:53 -0300

    ✔ Added: custom scrollbar

--- web/src/pages/Event/Steps/PickListStep.tsx
+++ web/src/pages/Event/Steps/PickListStep.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable tailwindcss/classnames-order */
 /* eslint-disable tailwindcss/no-custom-classname */
 import data from './list-mock.json';
 
@@ -7,10 +8,13 @@ export function PickListStep() {
       <h1 className="font-amatic text-4xl text-slate-50 sm:text-5xl">
         Gostaria de nos presentear com algum item da lista abaixo?
       </h1>
-      <div>
+      <div className="scrollbar-thumb-rounded-full scrollbar-track-rounded-full mt-12 max-h-80 max-w-full overflow-y-scroll scrollbar-thin scrollbar-track-slate-600 scrollbar-thumb-slate-800">
         <ul>
           {data.map((e, index) => (
-            <li key={index} className="font-satisfy text-3xl text-slate-50">
+            <li
+              key={index}
+              className="cursor-pointer font-satisfy text-3xl text-slate-50"
+            >
               {e.name}
             </li>
           ))}
