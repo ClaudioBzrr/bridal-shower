cf69fad58c90cfd1fa3e54de5177297bb9ff70cb
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-10-29 09:36:28 -0300

    ✔ Added: find items in items controller

---
+++ backend/src/controllers/item-controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { CreateItemUSeCase } from '../use-cases/create-item-use-case';
+import { PrismaUserRepository } from '../repositories/prisma/prisma-user-repository';
+import { PrismaItemRepository } from '../repositories/prisma/prisma-item-repository';
+import { ICreateItemPayload } from '../entities/Item';
+import { ListItemsUseCase } from '../use-cases/list-items-use-case';
+
+const userRepository = new PrismaUserRepository();
+const itemRepository = new PrismaItemRepository();
+
+export class IItemController {
+  async create(request: Request, response: Response) {
+    try {
+      const createItemUseCase = new CreateItemUSeCase(
+        itemRepository,
+        userRepository,
+      );
+      const payload: ICreateItemPayload = request.body;
+      const item = await createItemUseCase.exec(payload);
+      return response.json(item);
+    } catch (err) {
+      return response.status(404).json(err);
+    }
+  }
+  async find(request: Request, response: Response) {
+    try {
+      const listItemsUseCase = new ListItemsUseCase(itemRepository);
+      const items = listItemsUseCase.exec();
+      return response.json(items);
+    } catch (err) {
+      return response.status(404).json(err);
+    }
+  }
+}
