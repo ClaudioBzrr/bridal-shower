5f1286d22f967bcfdf4751baec1ead4bacaab2b4
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-10-29 09:40:12 -0300

    ✔ Added: pick function in item controller

--- backend/src/controllers/item-controller.ts
+++ backend/src/controllers/item-controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from 'express';
 import { CreateItemUSeCase } from '../use-cases/create-item-use-case';
 import { PrismaUserRepository } from '../repositories/prisma/prisma-user-repository';
 import { PrismaItemRepository } from '../repositories/prisma/prisma-item-repository';
-import { ICreateItemPayload } from '../entities/Item';
+import { ICreateItemPayload, IPickItemPayload } from '../entities/Item';
 import { ListItemsUseCase } from '../use-cases/list-items-use-case';
+import { PickItemUseCase } from '../use-cases/pick-item-use-case';
 
 const userRepository = new PrismaUserRepository();
 const itemRepository = new PrismaItemRepository();
@@ -27,6 +28,16 @@ export class IItemController {
       const listItemsUseCase = new ListItemsUseCase(itemRepository);
       const items = listItemsUseCase.exec();
       return response.json(items);
+    } catch (err) {
+      return response.status(404).json(err);
+    }
+  }
+  async pick(request: Request, response: Response) {
+    try {
+      const pickItemUseCAse = new PickItemUseCase(itemRepository);
+      const payload: IPickItemPayload = request.body;
+      const item = await pickItemUseCAse.exec(payload);
+      return response.json(item);
     } catch (err) {
       return response.status(404).json(err);
     }
