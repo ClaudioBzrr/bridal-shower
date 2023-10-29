6c5d51867bd5f85765869528b3f744707b0bd5fc
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-10-28 21:48:35 -0300

    ✔ Added: controllers

---
+++ backend/src/controllers/user-controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { ICreateUserPayload, ILoginPayload } from '../entities/User';
+import { CreateUserUseCase } from '../use-cases/create-user-use-case';
+import { PrismaUserRepository } from '../repositories/prisma/prisma-user-repository';
+import { LoginUseCase } from '../use-cases/login-use-case';
+
+const userRepository = new PrismaUserRepository();
+
+export class UserController {
+  async create(request: Request, response: Response) {
+    try {
+      const createUserUseCase = new CreateUserUseCase(userRepository);
+      const payload: ICreateUserPayload = request.body;
+      const user = await createUserUseCase.exec(payload);
+      return response.json(user);
+    } catch (err) {
+      return response.status(404).json(err);
+    }
+  }
+  async login(request: Request, response: Response) {
+    try {
+      const loginUseCase = new LoginUseCase(userRepository);
+      const payload: ILoginPayload = request.body;
+      const auth = await loginUseCase.exec(payload);
+      return response.json(auth);
+    } catch (err) {
+      return response.status(404).json(err);
+    }
+  }
+}
