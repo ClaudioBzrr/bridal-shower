d1dbafe2dc97f9d13ac345ddd7252d0848d33c30
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-11-05 11:57:50 -0300

    ✔ Added: finishedSubmit field in users

---
+++ backend/prisma/migrations/20231105145704_added_finished_submit_field/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN "finishedSubmit" BOOLEAN DEFAULT false;
--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -4,16 +4,20 @@ import { IForeign } from './system/Foreign';
 export type IUser = IAutogen & {
   name: string;
   role?: string | null;
+  finishedSubmit?: boolean;
   email: string;
   confirmedAttendance?: number | null;
 };
 
 export type ICreateUserPayload = Omit<
   IUser,
-  keyof IAutogen | keyof IForeign | 'role'
+  keyof IAutogen | keyof IForeign | 'role' | 'finishedSubmit'
 >;
 export type ICreateAdminPayload = {
-  payload: Omit<IUser, keyof IAutogen | keyof IForeign | 'role'> & {
+  payload: Omit<
+    IUser,
+    keyof IAutogen | keyof IForeign | 'role' | 'finishedSubmit'
+  > & {
     role: string;
   };
   authId?: IUser['id'];
