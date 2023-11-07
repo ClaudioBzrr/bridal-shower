5cb3d9602cf5c21d4c01adb00aef281bbdac41af
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-11-06 21:52:08 -0300

    ✔ Added: finished submit in use-cases

--- backend/src/use-cases/confirm-amount-presence-use-case.ts
+++ backend/src/use-cases/confirm-amount-presence-use-case.ts
@@ -5,6 +5,10 @@ export class ConfirmAmountPresenceUseCase {
   constructor(private userRepository: IUserRepository) {}
 
   async execute(payload: IConfirmPeopeAmountPresencePayload) {
+    const authUser = await this.userRepository.findOne({ id: payload.authId });
+    if (authUser.finishedSubmit == true) {
+      throw new Error('Usuário já finalizou o cadastro de informações');
+    }
     await this.userRepository.update(
       { id: payload.authId },
       { confirmedAttendance: payload.confirmedAttendance },
--- backend/src/use-cases/pick-item-use-case.ts
+++ backend/src/use-cases/pick-item-use-case.ts
@@ -1,10 +1,18 @@
 import { IPickItemPayload } from '../entities/Item';
 import { IItemRepository } from '../repositories/item-repository';
+import { IUserRepository } from '../repositories/user-repository';
 
 export class PickItemUseCase {
-  constructor(private itemRepository: IItemRepository) {}
+  constructor(
+    private itemRepository: IItemRepository,
+    private userRepository: IUserRepository,
+  ) {}
 
   async exec(payload: IPickItemPayload) {
+    const authUser = await this.userRepository.findOne({ id: payload.authId });
+    if (authUser.finishedSubmit == true) {
+      throw new Error('Usuário já finalizou o cadastro de informações');
+    }
     payload.itemId.map(async (e) => {
       const item = await this.itemRepository.findOne({ id: e });
       if (item.userId == null) {
