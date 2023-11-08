c8509aa48f44f879e5bbb633dfee8648c60211c9
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-11-07 23:18:42 -0300

    ✔ Added: register input fields as required

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -47,6 +47,7 @@ export function Register() {
       >
         <div className="w-full">
           <Input
+            required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Digite seu E-mail"
@@ -54,6 +55,7 @@ export function Register() {
         </div>
         <div className="mt-8 w-full">
           <Input
+            required
             value={name}
             onChange={(e) => setName(e.target.value)}
             placeholder="Digite seu Nome"
