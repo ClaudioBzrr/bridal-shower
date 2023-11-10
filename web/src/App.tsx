ace7c709e4df084f6a81f9d8ad3681f1aff62e9e
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-11-10 00:31:53 -0300

    ✔ Added: routing config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,12 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { routes } from './Routes';
 
 export function App() {
   return (
-    <BrowserRouter basename="/bridal-shower">
-      <Routes>
-        {routes.map(({ path, element }, index) => (
-          <Route key={index} path={path} element={element} />
-        ))}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {routes.map(({ path, element }, index) => (
+        <Route key={index} path={path} element={element} />
+      ))}
+    </Routes>
   );
 }
